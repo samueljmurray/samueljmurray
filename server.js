90d70092c40f713c63c3bcc1554760a884ffbe4b
Samuel Murray <samueljmurray@googlemail.com>
2013-04-21 14:34:38 +0100

    Fix server error

--- server.js
+++ server.js
@@ -4,8 +4,7 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
+  , routes = require('./routes/routes')
   , http = require('http')
   , path = require('path');
 
