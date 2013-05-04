1e382473a828543127a8ad163b49d9df84223117
Samuel Murray <samueljmurray@googlemail.com>
2013-05-04 15:54:56 +0100

    More front-end work

--- public/js/views/home.js
+++ public/js/views/home.js
@@ -8,6 +8,31 @@ window.HomeView = Backbone.View.extend({
 		var homeitems = this.model.models;
 		$(this.el).html(this.template(this.model.toJSON()));
 		return this;
-	}
+	},
+
+	events: {
+      "click .project-scroll-back"   : "projectScrollBack",
+      "click .project-scroll-forward" : "projectScrollForward"
+  },
+
+  projectScrollBack : function() {
+  	if (!window.projectspos) {
+  		window.projectspos = 0;
+  	}
+  	if (window.projectspos != 0) {
+  		window.projectspos -= 1;
+  		$("#projects-splash-wrapper").animate({left: '-' + window.projectspos + '00%'},500);
+  	}
+  },
+
+  projectScrollForward : function() {
+  	if (!window.projectspos) {
+  		window.projectspos = 0;
+  	}
+  	if (window.projectspos != 3) {
+  		window.projectspos += 1;
+  		$("#projects-splash-wrapper").animate({left: '-' + window.projectspos + '00%'},500);
+  	}
+  }
 
 })
